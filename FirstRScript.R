35bc22246367f6a201bf3a7fc9ca283333565298
Ryan Heines <ryheines@gmail.com>
2022-07-13 11:32:55 -0400

    modify and add fmd

---
+++ FirstRScript.R
@@ -0,0 +1,11 @@
+# Description of script content
+# 12 July 2022
+# modified on _
+# RCH
+
+rainfall <- runif(10)
+biomass <- runif(10)
+print(rainfall)
+rainfall
+head(rainfall)
+plot(x=rainfall, y=biomass)
